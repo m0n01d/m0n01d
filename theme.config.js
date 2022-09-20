3c4a7be6daa8ecd4b54d024e5a643397a4cf50a8
Dwight <dwightdoane@me.com>
2022-09-20 00:55:25 -0400

    miata begins

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,7 @@
 const YEAR = new Date().getFullYear()
 
 export default {
+  readMore: <p style={{ marginLeft: '1rem' }}>Read More →</p>,
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Dwight Doane
@@ -18,5 +19,29 @@ export default {
       `}</style>
     </small>
   ),
-  darkMode: true
+  head: (
+    <>
+      <meta name="hello" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/favicon_io/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon_io/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon_io/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/images/favicon_io/site.webmanifest" />
+    </>
+  ),
+  darkMode: true,
+  font: false
 }
